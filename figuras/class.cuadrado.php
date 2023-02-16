aa021110c27b1584671cfe818b129926e66e4317
Rafael Fonseca <117616632+8444-arfonseca1@users.noreply.github.com>
2023-02-16 13:11:38 -0500

    Update class.cuadrado.php

--- figuras/class.cuadrado.php
+++ figuras/class.cuadrado.php
@@ -1,28 +1,39 @@
 <?php
 class cuadrado extends figura implements formulas {
+
+    private $lado1;
+    private $tipo;
+
+
+    public function __construct($lado1, $tipo)
+    {
+        $this->lado1 = $lado1;
+        $this->tipo = $tipo;
+    }
+
+
     public function GetArea()
     {
-
+        return $this->lado1^2;
     }
 
     public function GetPerimetro()
     {
-
+        return $this->lado1*4;
     }
 
     public function GetTipo()
     {
-
+        return $this->tipo;
     }
 
     public function area()
     {
-        echo "Hola Area";
+        echo "El área del" . $this->GetTipo() . "es: " . $this->GetArea();
     }
 
     public function perimetro()
     {
-        echo "Hola Perimetro";
+        echo "El perímetro del" . $this->GetTipo() . "es: " . $this->GetPerimetro();
     }
 }
-?>
