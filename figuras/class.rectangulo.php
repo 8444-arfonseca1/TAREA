6b63daf31f50946d9d4bc19915ce386144a92938
Rafael Fonseca <117616632+8444-arfonseca1@users.noreply.github.com>
2023-02-16 13:11:42 -0500

    Update class.rectangulo.php

--- figuras/class.rectangulo.php
+++ figuras/class.rectangulo.php
@@ -1,28 +1,41 @@
 <?php
 class rectangulo extends figura implements formulas{
+
+    private $lado1;
+    private $lado2;
+    private $tipo;
+
+
+    public function __construct($lado1, $lado2, $tipo)
+    {
+        $this->lado1 = $lado1;
+        $this->lado2 = $lado2;
+        $this->tipo = $tipo;
+    }
+
+
     public function GetArea()
     {
-
+        return $this->lado1 * $this->lado2;
     }
 
     public function GetPerimetro()
     {
-
+        return ($this->lado1 * 2) + ($this->lado2 * 2);
     }
 
     public function GetTipo()
     {
-
+        return $this->tipo;
     }
 
     public function area()
     {
-        echo "Hola Area";
+        echo "El área del" . $this->GetTipo() . " es: " . $this->GetArea();
     }
 
     public function perimetro()
     {
-        echo "Hola Perimetro";
+        echo "El perímetro del" . $this->GetTipo() . "es: " . $this->GetPerimetro();
     }
 }
-?>
